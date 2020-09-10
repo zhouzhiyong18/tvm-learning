87e3b7d067dae08dc19b029b7cbe5407ab022c55
zhouzhiyong <438504124@qq.com>
2020-09-10 16:51:45 +0800

    [Fix]tvm to tvm.te

--- scheduling/schedule stage/API intro/split.py
+++ scheduling/schedule stage/API intro/split.py
@@ -1,12 +1,12 @@
 import tvm
 
 n = 1024
-A = tvm.placeholder((n,), name='A')
-k = tvm.reduce_axis((0, n), name='k')
+A = tvm.te.placeholder((n,), name='A')
+k = tvm.te.reduce_axis((0, n), name='k')
 
-B = tvm.compute((1,), lambda i: tvm.sum(A[k], axis=k), name='B')
+B = tvm.te.compute((1,), lambda i: tvm.te.sum(A[k], axis=k), name='B')
 
-s = tvm.create_schedule(B.op)
+s = tvm.te.create_schedule(B.op)
 
 print(tvm.lower(s, [A, B], simple_mode=True))
 print("---------cutting line---------")
